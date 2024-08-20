8886e9f7e515ad4f569fb4b5a1bafca7c7fb816e
shuheix <ono.shuhei210@gmail.com>
2024-08-20 22:02:23 +0900

    feat: dir作成

---
+++ infrastructure/router/router.go
@@ -0,0 +1,13 @@
+package router
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func SetUpRoutes(e *echo.Echo) {
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+}
--- main.go
+++ main.go
@@ -1,20 +1,16 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4/middleware"
-	"net/http"
-
 	"github.com/labstack/echo/v4"
+	"k3_api/infrastructure/middleware"
+	"k3_api/infrastructure/router"
 )
 
 func main() {
 	e := echo.New()
 
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
+	middleware.SetUpMiddleware(e)
+	router.SetUpRoutes(e)
 
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
